566baa72d18cacc2e42779961daad3fcc665346e
Max Maton <max@maton.info>
2014-02-01 19:50:40 +0100

    Added NO_MULTITHEAD configuration

--- HiPerf/ApplyManager.cs
+++ HiPerf/ApplyManager.cs
@@ -6,40 +6,79 @@ using System.Threading.Tasks;
 
 namespace HiPerf
 {
+    /// <summary>
+    /// Holds references to Applyable objects and applies changes concurrently
+    /// </summary>
+    /// <typeparam name="T">The type of applyable objects to use</typeparam>
     public class ApplyManager<T> : IEnumerable<T> where T : Applyable
     {
+        /// <summary>
+        /// The list of Applyable instances
+        /// </summary>
         List<T> _members = new List<T>();
 
+        /// <summary>
+        /// The number of Applyable instances
+        /// </summary>
         public int Count { get { return _members.Count; } }
 
+        /// <summary>
+        /// Returns an enumerator over the Applyable Instances
+        /// </summary>
+        /// <returns>An enumerator</returns>
         public IEnumerator<T> GetEnumerator()
         {
             return _members.GetEnumerator();
         }
 
+        /// <summary>
+        /// Returns an enumerator over the Applyable Instances
+        /// </summary>
+        /// <returns>An enumerator</returns>
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Integrates all changes concurrently for all instances
+        /// </summary>
         public void Apply()
         {
-#if CHECK_APPLY
-            lock(_members)
+            ForAll(a => a.Apply());
+        }
+        
+        /// <summary>
+        /// Runs a function concurrently for all instances
+        /// </summary>
+        /// <param name="action">The function to run</param>
+        public void ForAll(Action<T> action)
+        {
+            
+#if NO_MULTITHREAD || CHECK_APPLY
+            lock (_members)
                 foreach (var m in _members)
-                    m.Apply();
+                    action(m);
 #else
             lock (_members)
-                Parallel.ForEach(_members, m => m.Apply());
+                Parallel.ForEach(_members, action);
 #endif
         }
 
+        /// <summary>
+        /// Adds a new instance to the manager
+        /// </summary>
+        /// <param name="member">The instance to add</param>
         public void Add(T member)
         {
             lock(_members)
                 _members.Add(member);
         }
 
+        /// <summary>
+        /// Removes an instance from the manager
+        /// </summary>
+        /// <param name="member">The instance to remove</param>
         public void Remove(T member)
         {
             lock (_members)
